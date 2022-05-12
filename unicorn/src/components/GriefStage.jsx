909fdb5e4796e881d058790351c3948281515ee6
Chinwendu <chinwenduukoha@gmail.com>
2022-05-11 23:49:09 -0400

    grief stage card init

--- unicorn/src/components/GriefStage.jsx
+++ unicorn/src/components/GriefStage.jsx
@@ -1,8 +1,31 @@
-import {useNavigate} from 'react-router-dom'
-import Modal from './Modal'
+import {useNavigate, Link} from 'react-router-dom'
 
 const GriefStage = ({griefStage, title, description, image, unicornUser, setUnicornUser, errorMessage, setErrorMessage, openModal, setOpenModal, header, setHeader }) => {
-  return <div className=""></div>
+  
+  let navigate = useNavigate()
+
+  const userAccess = (id) => {
+    if(unicornUser){
+      navigate(`/detail/grief-stage/${id}`)
+    }
+  }
+  
+  return(
+  <div className="grief-stage">
+    <div className='grief-stage-container'>
+      <Link to={`griefstage/${griefStage}`}>
+        <div className='grief-stage-card'>
+          <img className='grief-image' src={image}/>
+          <div className='card-info'>
+            <div className='card-btn'>
+              <button className='grief-stage-btn'>{title}</button>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  </div>
+  )
 }
 
 export default GriefStage
