88dff726051edb578b5c6aa3436f54e3f3e5e55b
Chinwendu <chinwenduukoha@gmail.com>
2022-05-15 23:54:15 -0400

    changes to nav, login and register

--- unicorn/src/components/Nav.jsx
+++ unicorn/src/components/Nav.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom"
 import Logo from '../assets/UnicornMainLogo.png'
 
-const Nav = ({authenticated, unicornUser, handleLogout}) => {
-  let authenticatedOptions
-  if(unicornUser) {
-    authenticatedOptions= (
+const Nav = () => {
+
+  return (
     <nav className="navbar-text nav-body">
       <div className="welcome">
-          <h1 className="username">Welcome {unicornUser.username}!</h1>
+          <h1 className="username">Welcome to Unicorn!</h1>
           <ul className="icon icon-container">
           <input type='checkbox' id="hamburger_icon" />
           <label htmlFor='hamburger_icon' className="hamburger">&#9776;</label>
           <div className="nav-menu" >
 
             <div className="icon icon-fill">
-              <li className="i fa fa-home"><Link to='/home'>Home</Link></li>
+              <li className="i fa fa-home"><Link to='/'>Home</Link></li>
+            </div>
+
+            <div className="icon icon-fill">
+              <li className="i fa fa-home"><Link to='/feed/:id'>feed</Link></li>
             </div>
 
             <div className="icon icon-fill">
@@ -37,111 +40,6 @@ const Nav = ({authenticated, unicornUser, handleLogout}) => {
         </ul>
       </div>
     </nav>
-  )
-  }
-  let adminOptions
-  if (unicornUser) {
-   adminOptions = (
-     <nav className="navbar-text nav-body">
-       <div className="welcome" > 
-         <h1 className="username">
-         Welcome {unicornUser.username}!
-         </h1>
-
-         <ul className="icon icon-container">
-         <input type='checkbox' id="hamburger_icon" />
-         <label htmlFor='hamburger_icon' className="hamburger">&#9776;</label>
-         <div className="nav-menu" >
-           <div className="icon icon-fill">
-               <li className="i fa fa-home"><Link to='/home'>Home</Link></li>
-             </div>
-
-             <div className="icon icon-fill">
-              <li className="i fa fa-home"><Link to='/about'>About Unicorn</Link></li>
-            </div>
-
-             <div className="icon icon-enter">
-             <li className="fa fa-user"><Link to="/profile">My Profile</Link></li>
-           </div>
-
-           <div className="icon icon-enter">
-              <li className="fa fa-user"><Link to="/stages">Grief Stages</Link></li>
-            </div>
-
-            <div className="icon icon-enter">
-              <li className="fa fa-user"><Link to="/resources">Resources</Link></li>
-            </div>
-
-             <div className="icon icon-collapse">
-               <li className="i fa fa-sign-in"><Link onClick={handleLogout} to='/'>Sign Out</Link></li>
-             </div>
-           
-         </div>
-       </ul>
-
-         
-       </div>
-       
-     </nav>
-   )
-  }
-
-  const publicOptions = (
-
-    <nav className="navbar-text nav-body">
-    <div className="welcome" > 
-      <h1 className="username">
-      Welcome {unicornUser.username}!
-      </h1>
-
-      <ul className="icon icon-container">
-      <input type='checkbox' id="hamburger_icon" />
-      <label htmlFor='hamburger_icon' className="hamburger">&#9776;</label>
-      <div className="nav-menu" >
-        <div className="icon icon-fill">
-            <li className="i fa fa-home"><Link to='/home'>Home</Link></li>
-          </div>
-
-          <div className="icon icon-fill">
-           <li className="i fa fa-home"><Link to='/about'>About Unicorn</Link></li>
-         </div>
-
-         <div className="icon icon-enter">
-           <li className="fa fa-user"><Link to="/resources">Resources</Link></li>
-         </div>
-
-         <div className="icon icon-enter">
-           <li className="fa fa-user"><Link to="/register">Register</Link></li>
-         </div>
-
-         <div className="icon icon-enter">
-           <li className="fa fa-user"><Link to="/login">Sign In</Link></li>
-         </div>
-
-          <div className="icon icon-collapse">
-            <li className="i fa fa-sign-in"><Link onClick={handleLogout} to='/'>Sign Out</Link></li>
-          </div>
-        
-      </div>
-    </ul>
-
-      
-    </div>
-    
-  </nav>
-  )
-
-  return (
-    <header className="navbar container" >
-      <div className="nav-left content">
-      <Link to='/feed/:id' >
-        <div className="logo-container" alt='logo' >
-          <img className="logo" src={Logo} alt="unicornUser-logo" />
-        </div>
-      </Link>
-      </div>
-      {authenticated && unicornUser ? (unicornUser.admin === true ? adminOptions: authenticatedOptions) : publicOptions}
-    </header>
   )
 }
 
